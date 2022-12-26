15af90f32eca070d7c2491bd26efa5727d18acf9
Jason Hamm <jason.robert.hamm@gmail.com>
2022-12-26 10:48:01 -0500

    Remap telescope fuzzy

--- lua/hamm/core/keymaps.lua
+++ lua/hamm/core/keymaps.lua
@@ -33,7 +33,7 @@ keymap.set("n", "<c-b>", ":NvimTreeToggle<CR>")
 
 -- *** fuzzy search ***
 -- plugin: telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<c-p>", "<cmd>Telescope find_files<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
