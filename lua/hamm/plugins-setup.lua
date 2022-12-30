348f931ff43a0f280a257a959386b9460cabb28c
Jason Hamm <jason.robert.hamm@gmail.com>
2022-12-30 17:20:24 -0500

    Install cmp-nvim-lua

--- lua/hamm/plugins-setup.lua
+++ lua/hamm/plugins-setup.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
 	use("hrsh7th/nvim-cmp")
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lua")
 
 	-- snippets
 	use("L3MON4D3/LuaSnip")
