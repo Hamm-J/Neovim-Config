ee643b0111473174a620ea37bcd11f8c7e3cdaeb
Jason Hamm <jason.robert.hamm@gmail.com>
2023-03-09 23:34:42 -0500

    Restyle nvim-dap breakpoint icon

--- lua/hamm/core/colors.lua
+++ lua/hamm/core/colors.lua
@@ -17,3 +17,51 @@ vim.cmd([[ hi TelescopeSelectionCaret guibg=#1c1e26 ctermbg=gray ]])
 -- vim.cmd([[ hi lualine_a_buffers_active guifg=#c8ccd4 ]])
 
 vim.cmd([[ hi lualine_a_buffers_inactive guibg=#1d1f27 ]])
+
+-- plugin: nvim-dap
+-- vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+-- vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+-- vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "", bg = "#31353f" })
+
+vim.fn.sign_define(
+	"DapBreakpoint",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointCondition",
+	{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapBreakpointRejected",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapLogPoint",
+	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+-- vim.api.nvim_set_hl(0, "blue", { fg = "#3d59a1" })
+-- vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
+-- vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" })
+-- vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" })
+--
+-- vim.fn.sign_define(
+-- 	"DapBreakpoint",
+-- 	{ text = "•", texthl = "blue", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+-- )
+-- vim.fn.sign_define(
+-- 	"DapBreakpointCondition",
+-- 	{ text = "•", texthl = "blue", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+-- )
+-- vim.fn.sign_define(
+-- 	"DapBreakpointRejected",
+-- 	{ text = "•", texthl = "orange", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+-- )
+-- vim.fn.sign_define("DapStopped", { text = "•", texthl = "green", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+-- vim.fn.sign_define(
+-- 	"DapLogPoint",
+-- 	{ text = "•", texthl = "yellow", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+-- )
