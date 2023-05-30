e6c7f71975728dbdfe5a3dcaf80102f00e1c58ab
Jason Hamm <jason.robert.hamm@gmail.com>
2023-05-29 21:33:56 -0400

    disable treeview for netrw

--- lua/hamm/core/options.lua
+++ lua/hamm/core/options.lua
@@ -49,6 +49,6 @@ opt.iskeyword:append("-")
 
 -- netrw
 vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
+-- vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 40
 vim.g.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
