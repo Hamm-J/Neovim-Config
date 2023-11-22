cce1153fe56c5f2f909c9627d0f7827d6a273148
Jason Hamm <jason.robert.hamm@gmail.com>
2023-11-21 22:03:18 -0500

    refactor: disable auto formatting and create keybind for formatting

--- lua/hamm/plugins/lsp/lsp-zero.lua
+++ lua/hamm/plugins/lsp/lsp-zero.lua
@@ -94,6 +94,9 @@ lsp.nvim_workspace()
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
+	vim.keymap.set("n", "<leader>f", function()
+		vim.lsp.buf.format()
+	end, opts)
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
 	end, opts)
