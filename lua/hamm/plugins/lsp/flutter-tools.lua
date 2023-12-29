8db872433881993493e5295b1432df6d8fdb5b43
Jason Hamm <jason.robert.hamm@gmail.com>
2023-12-29 10:53:51 -0500

    refactor: update flutter-tools config

--- lua/hamm/plugins/lsp/flutter-tools.lua
+++ lua/hamm/plugins/lsp/flutter-tools.lua
@@ -1,13 +1,25 @@
 local status, flutter_tools = pcall(require, "flutter-tools")
 if not status then
-	return
+    return
 end
 
 flutter_tools.setup({
     lsp = {
         color = {
             enabled = true,
-            background = false 
-        }
+            background = false,
+            virtual_text = false,
+        },
+    },
+    closing_tags = {
+        enabled = true,
+    },
+    widget_guides = {
+        enabled = true,
+    },
+    settings = {
+        showTodos = false,
+        -- showCompleteFunctionCalls = false,
+        -- enableSnippets = false,
     }
 })
