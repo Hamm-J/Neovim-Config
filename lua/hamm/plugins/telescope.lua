466357d97d9d198c9e68a94c8bbf3cc40cfd9b41
Jason Hamm <jason.robert.hamm@gmail.com>
2024-01-15 21:02:41 -0500

    refactor: use normal mode by default for telescope and update telescope keybinds

--- lua/hamm/plugins/telescope.lua
+++ lua/hamm/plugins/telescope.lua
@@ -11,6 +11,7 @@ end
 telescope.setup({
 	pickers = {
 		find_files = {
+            initial_mode = "normal",
 			-- hidden = true,
 			-- show dotfiles besides .git folder
 			find_command = {
@@ -21,6 +22,24 @@ telescope.setup({
 				"!.git",
 			},
 		},
+        resume = {
+            initial_mode = "normal",
+        },
+        live_grep = {
+            initial_mode = "normal",
+        },
+        grep_string = {
+            initial_mode = "normal",
+        },
+        buffers = {
+            initial_mode = "normal",
+        },
+        current_buffer_fuzzy_find = {
+            initial_mode = "normal",
+        },
+        help_tags = {
+            initial_mode = "normal",
+        },
 	},
 	defaults = {
 		mappings = {
@@ -28,10 +47,10 @@ telescope.setup({
 				["<C-k>"] = actions.move_selection_previous,
 				["<C-j>"] = actions.move_selection_next,
 				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-				["<c-d>"] = actions.delete_buffer,
+				["<c-q>"] = actions.delete_buffer,
 			},
 			n = {
-				["<C-d>"] = actions.delete_buffer,
+				["<C-q>"] = actions.delete_buffer,
 				["<S-j>"] = actions.cycle_history_next,
 				["<S-k>"] = actions.cycle_history_prev,
 			},
